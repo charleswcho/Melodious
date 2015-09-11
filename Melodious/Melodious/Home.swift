4125d12e4f9042bc74c7294e48692bde76aa3e9b
Charles Wesley Cho <charleswcho@gmail.com>
2015-09-11 11:29:06 -0700

    Create UI

---
+++ Melodious/Melodious/Home.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Melodious
+//
+//  Created by Charles Wesley Cho on 9/11/15.
+//  Copyright (c) 2015 Charles Wesley Cho. All rights reserved.
+//
+
+import UIKit
+
+class Home: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
--- Melodious/Melodious/NewGame.swift
+++ Melodious/Melodious/NewGame.swift
@@ -1,5 +1,5 @@
 //
-//  FirstViewController.swift
+//  NewGame.swift
 //  Melodious
 //
 //  Created by Charles Wesley Cho on 9/11/15.
@@ -8,18 +8,18 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class NewGame: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
 
 }
-
