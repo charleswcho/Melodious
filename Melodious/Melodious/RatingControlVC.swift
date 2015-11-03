40478ca7489723f87152840f5a24100f7b468cec
Charles Wesley Cho <charleswcho@gmail.com>
2015-11-02 19:10:22 -0800

    Add Star ratings

---
+++ Melodious/Melodious/RatingControlVC.swift
@@ -0,0 +1,20 @@
+//
+//  RatingControlView.swift
+//  Melodious
+//
+//  Created by Charles Wesley Cho on 11/2/15.
+//  Copyright © 2015 Charles Wesley Cho. All rights reserved.
+//
+
+import UIKit
+
+class RatingControlView: UIView {
+
+    // MARK: Initialization
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+    }
+
+}
